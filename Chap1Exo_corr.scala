9a72239a33ccd1224118e91c29ff2a7ef9098a97
DJEBALI Wissam <wolusien@users.noreply.github.com>
2019-01-23 11:13:42 +0100

    Add files via upload

---
+++ Chap1Exo_corr.scala
@@ -0,0 +1,165 @@
+import scala.math.BigInt
+
+object Chap1Exo_corr{
+
+  /**
+   * Task 1:
+   *
+   * In the Scala REPL, type `3.` followed by the Tab key. What methods can be applied?
+   *
+   * Solution:
+   *
+   * On my machine, when typing `3.` and pressing the Tab key for the first time,
+   * these methods are available:
+   * {{{
+   * %   &   *   +   -   /   >   >=   >>   >>>   ^   asInstanceOf   isInstanceOf
+   * toByte   toChar   toDouble   toFloat   toInt   toLong   toShort   toString
+   * unary_+   unary_-   unary_~   |
+   * }}}
+   * And when pressing the Tab key the second time:
+   * {{{
+   * !=   %   *   -   <    <=       ==   >=   >>>   asInstanceOf   getClass   isInstanceOf
+   * toChar     toFloat   toLong    toString   unary_-   |
+   * ##   &   +   /   <<   <init>   >    >>   ^     equals         hashCode
+   * toByte         toDouble   toInt     toShort   unary_+    unary_~
+   * }}}
+   */
+
+  /**
+   * Task 2:
+   *
+   * In the Scala REPL, compute the square root of 3, and then square that value.
+   * By how much does the result differ from 3? (Hint: The res variables are your friend.)
+   *
+   * Solution:
+   *
+   * {{{
+   * scala> Math.sqrt(3)
+   * res0: Double = 1.7320508075688772
+   *
+   * scala> res0 * res0
+   * res1: Double = 2.9999999999999996
+   *
+   * scala> 3 - res1
+   * res2: Double = 4.440892098500626E-16
+   * }}}
+   */
+
+  /**
+   * Task 3:
+   *
+   * Are the res variables `val` or `var` ?
+   *
+   * Solution:
+   *
+   * They are `val`:
+   * {{{
+   * scala> res2 = 1.0
+   * <console>:13: error: reassignment to val
+   *        res2 = 1.0
+   *             ^
+   * }}}
+   */
+
+  /**
+   * Task 4:
+   *
+   * Scala lets you multiply a string with a number - try out `"crazy" * 3` in the REPL.
+   * What does this operation do? Where can you find it in `Scaladoc`?
+   *
+   * Solution:
+   *
+   * This operation concatenates the given string to itself the given number of times:
+   * {{{
+   * scala> "crazy" * 3
+   * res3: String = crazycrazycrazy
+   * }}}
+   * It can be found in `Scaladoc` by searching for `StringOps` class, it has the following
+   * method:
+   * {{{
+   * def *(n: Int): String
+   * Return the current string concatenated n times.
+   * }}}
+   */
+
+  /**
+   * Task 5:
+   *
+   * What does `10 max 2` mean? In which class is the `max` method defined?
+   *
+   * Solution:
+   *
+   * It compares two integer values and returns the maximum value.
+   * The `max` method is defined in `RichInt` class.
+   * The `Int` value `10` is first converted to a `RichInt` and then the `max` method is called
+   * on that value.
+   */
+
+  /**
+   * Task 6:
+   *
+   * Using `BigInt`, compute `2^^1024`.
+   */
+  def computeBigInt(): BigInt = BigInt(2).pow(1024)
+
+  /**
+   * Task 7:
+   *
+   * What do you need to `import` so that you can get a random prime as
+   * `probablePrime(100, Random)`, without any qualifiers before `probablePrime` and `Random`?
+   */
+  def task7(): BigInt = {
+    import scala.BigInt._
+    import scala.util._
+
+    probablePrime(100, Random)
+  }
+
+  /**
+   * Task 8:
+   *
+   * One way to create random file or directory names is to produce a random `BigInt`
+   * and convert it to base `36`, yielding a `string` such as "qsnvbevtomcj38o06kul".
+   * Poke around `Scaladoc` to find a way of doing this in Scala.
+   */
+  def randomFileName(): String = util.Random.alphanumeric.take(20).mkString.toLowerCase
+
+  /**
+   * Task 9:
+   *
+   * How do you get the first character of a string in Scala? The last character?
+   */
+  def task9(): Unit = {
+    val str = "Some string"
+
+    val first = str(0)
+    val first2 = str.head
+
+    val last = str(str.length - 1)
+    val last2 = str.last
+  }
+
+  /**
+   * Task 10:
+   *
+   * What do the `take`, `drop`, `takeRight`, and `dropRight` string functions do?
+   * What advantage or disadvantage do they have over using `substring`?
+   *
+   * Solution:
+   *
+   * The advantage is that they don't throw `IndexOutOfBoundsException`.
+   * See examples below:
+   */
+  def task10(): Unit = {
+    val str = "Some string"
+
+    val first10 = str.take(15) // "Some string", no IndexOutOfBoundsException exception !
+    val empty = "".drop(5) // "", no IndexOutOfBoundsException exception !
+
+    val take = str.take(4) // "Some"
+    val drop = str.drop(5) // "string"
+
+    val takeRight = str.takeRight(6) // "string"
+    val dropRight = str.dropRight(7) // "Some"
+  }
+}
---
+++ Chap2Exo_corr.scala
@@ -0,0 +1,122 @@
+import scala.math.Numeric.IntIsIntegral
+
+object Chap2Exo_corr{
+
+  /**
+   * Task 1:
+   *
+   * The `signum` of a number is `1` if the number is positive, `–1` if it is negative,
+   * and `0` if it is zero. Write a function that computes this value.
+   */
+  def signum(n: Int): Int = {
+    if (n < 0) -1
+    else if (n > 0) 1
+    else 0
+  }
+
+  /**
+   * Task 2:
+   *
+   * What is the value of an empty block expression `{}`? What is its type?
+   *
+   * Solution:
+   *
+   * The value of an empty block expression `{}` is `()` and it's type is `scala.Unit`.
+   * Which is equivalent to `java.lang.Void`.
+   */
+  def task2(): Unit = {}
+
+  /**
+   * Task 3:
+   *
+   * Come up with one situation where the assignment `x = y = 1` is valid in Scala.
+   * (Hint: Pick a suitable type for `x`.)
+   */
+  def task3(): Unit = {
+    var y: Int = 0
+    var x: Unit = ()
+    x = y = 1
+  }
+
+  /**
+   * Task 4:
+   *
+   * Write a `Scala` equivalent for the `Java` loop
+   * {{{
+   * for (int i = 10; i >= 0; i--) System.out.println(i);
+   * }}}
+   */
+  def task4(): Unit = for (i <- 10 to (0, -1)) println(i)
+
+  /**
+   * Task 5:
+   *
+   * Write a procedure `countdown(n: Int)` that prints the numbers from n to 0.
+   */
+  def countdown(n: Int): Unit = for (i <- n to (0, -1)) println(i)
+
+  /**
+   * Task 6:
+   *
+   * Write a `for` loop for computing the product of the Unicode codes of all letters
+   * in a string. For example, the product of the characters in "Hello" is 825152896.
+   */
+  def productLoop(s: String): Int = {
+    var result = 1
+    for (c <- s) result *= c.toInt
+
+    result
+  }
+
+  /**
+   * Task 7:
+   *
+   * Solve the preceding exercise without writing a loop.
+   * (Hint: Look at the `StringOps` Scaladoc.)
+   */
+  def productNoLoop(s: String): Int = s.map(_.toInt).product
+
+  /**
+   * Task 8:
+   *
+   * Write a function `product(s : String)` that computes the product, as described
+   * in the preceding exercises.
+   */
+  def product(s : String): Int = productLoop(s)
+
+  /**
+   * Task 9:
+   *
+   * Make the function of the preceding exercise a recursive function.
+   */
+  def productRecursive(s : String): Int = {
+    if (s.isEmpty) {
+      return 1
+    }
+
+    s.head.toInt * productLoop(s.tail)
+  }
+
+  /**
+   * Task 10:
+   *
+   * Write a function that computes `x^^n`, where `n` is an integer. Use the following
+   * recursive definition:
+   * {{{
+   * x^n = y^2 if n is even and positive, where y = x^(n / 2).
+   * x^n = x * x^(n – 1) if n is odd and positive.
+   * x^0 = 1.
+   * x^n = 1 / (x^–n) if n is negative.
+   * }}}
+   * Don't use a `return` statement.
+   */
+  def pow(x: Int, n: Int): Double = {
+    if (n == 0) 1
+    else if (n < 0) 1 / pow(x, -n)
+    else if (n % 2 != 0) x * pow(x, n - 1)
+    else {
+      val y = pow(x, n / 2)
+      y * y
+    }
+  }
+}
